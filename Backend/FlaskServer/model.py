d3b92d3436b3593af6ac5f0030b6158e9eb140d1
Vikram <vikram6jayanth3@gmail.com>
2023-07-12 13:24:13 +0530

    Fixed some bugs in authentication

--- Backend/FlaskServer/model.py
+++ Backend/FlaskServer/model.py
@@ -4,6 +4,7 @@ from flask_cors import cross_origin
 
 from flask import Blueprint, request
 
+API_KEY = "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"
 API_URL = "https://api-inference.huggingface.co/models/google/pegasus-cnn_dailymail"
 headers = {"Authorization": f"Bearer {API_KEY}"}
 
@@ -13,12 +14,11 @@ model_bp = Blueprint("model", __name__)
 
 
 @cross_origin
-@model_bp.route("/model", methods = ["POST"])
 @login_required
+@model_bp.route("/model", methods = ["POST"])
 def model():
     data = request.get_json()
     payload = {"inputs":data["text"]}
 
     res = req.post(API_URL, headers=headers, json=payload)
-    print(res.json()[0]["summary_text"])
     return res.json()[0]["summary_text"]
